a4c2bb47a30c4cf7baba2af52ed27c998ed589e8
InesBafaluy <137764328+InesBafaluy@users.noreply.github.com>
2025-09-30 08:59:54 +0000

    Add user class and users data base

---
+++ users_db.py
@@ -0,0 +1,42 @@
+# Create a json file database to store users information
+
+import json
+import os       
+
+class UsersDatabase:
+    def __init__(self, db_file='users_database.json'):
+        self.db_file = db_file
+        self.users = self.load_users()
+
+    def load_users(self):
+        if os.path.exists(self.db_file):
+            with open(self.db_file, 'r') as file:
+                return json.load(file)
+        return {}
+
+    def save_users(self):
+        with open(self.db_file, 'w') as file:
+            json.dump(self.users, file, indent=4)
+
+    def add_user(self, username, password):
+        if username in self.users:
+            raise ValueError("User already exists")
+        self.users[username] = {'password': password}
+        self.save_users()
+
+    def remove_user(self, username):
+        if username not in self.users:
+            raise ValueError("User does not exist")
+        del self.users[username]
+        self.save_users()
+
+    def authenticate_user(self, username, password):
+        if username in self.users and self.users[username]['password'] == password:
+            return True
+        return False
+    
+    def unique_user(self, username):
+        return username not in self.users
+
+    def get_all_users(self):
+        return list(self.users.keys())
