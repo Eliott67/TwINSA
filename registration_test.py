ecd0d561dab7a53bf0c21beda45e2ed56ac2d265
perine <perine.gabarret@gmail.com>
2025-10-06 17:04:50 +0200

    maj

--- registration_test.py
+++ registration_test.py
@@ -1,72 +1,86 @@
-from registration import registration, users_db
+from registration import validate_registration, users_db
+from user import User
 
-# PROBLEME : add_user prend que 2 arguments or on en donne 4 dans les tests
-
-def reset_users_db():
-    users_db.users = []
 
 # Success 
 print("Test 1 - Success :")
-reset_users_db()
-users_db.add_user("test@example.com", "mat", "Pass123!", "Pass123!")
-print(registration())
-print()
+users_db.users = {}
+u = User("mat", "test@example.com", "Pass123!", "Mat", 20, "France")
+valid, msg = validate_registration(u, "Pass123!")
+assert valid is True
+assert msg == "Registration successful."
 
 # Missing fields
 print("Test 2 - Missing fields :")
-reset_users_db()
-users_db.add_user("test1@example.com", "mat01", "Pass234!", "")
-print(registration())
-reset_users_db()
-users_db.add_user("", "mat", "Pass234!", "Pass234!")
-print(registration())
-reset_users_db()
-users_db.add_user("test1@example.com", "", "Pass234!", "Pass234!")
-print(registration())
-print()
+users_db.users = {}
+u = User("", "test1@example.com", "Pass123!", "Mat01", 20, "France")
+valid, msg = validate_registration(u, "Pass123!")
+assert valid is False
+assert msg == "All fields are required."
+
+users_db.users = {}
+u=User("mat1", "", "Pass123!", "Mat1", 20, "France")
+valid, msg = validate_registration(u, "Pass123!")
+assert valid is False
+assert msg == "All fields are required."
+
+users_db.users = {}
+u=User("mat2", "test2@example.com", "", "Mat2", 20, "France")
+valid, msg = validate_registration(u, "")
+assert valid is False
+assert msg == "All fields are required."
+
 
 # Invalid email format
 print("Test 3 - Invalid email format :")
-reset_users_db()
-users_db.add_user("test2example.com", "mat02", "Pass234!", "Pass234!")
-print(registration())
-print()
+users_db.users = {}
+u = User("mat3", "invalidemail", "Pass123!", "Mat3", 20, "France")
+valid, msg = validate_registration(u, "Pass123!")
+assert valid is False
+assert msg == "Invalid email format."
 
 # Passwords do not match
-print("Test 4 - Passwords do not match :")
-reset_users_db()
-users_db.add_user("test3@example.com", "mat03", "Pass234!", "Pass123!")
-print(registration())
-print()
+#print("Test 4 - Passwords do not match :")
+#users_db.users = {}
+
 
 # Weak password
 print("Test 5 - Weak password :")
-reset_users_db()
-users_db.add_user("test4@example.com", "mat04", "pass234", "pass234")
-print(registration())
-print()
+users_db.users = {}
+u = User("mat4", "test4@example.com", "weak", "Mat4", 20, "France")
+valid, msg = validate_registration(u, "weak")
+assert valid is False
+assert msg == "Password must be at least 8 characters long and include uppercase, lowercase, digit, and special character."
 
 # Duplicate email
-print("Test 6 - Duplicate email :")
-reset_users_db()
-users_db.add_user("mat05", "Pass234!")
-print(registration())
-print() 
+#print("Test 6 - Duplicate email :")
+#users_db.users = {}
+#u=User("mat5", "test@example.com", "Pass234!", "Mat5", 20, "France")
+#valid, msg = validate_registration(u, "Pass234!")
+#assert valid is False    
+#assert msg == "Email already exists. Please choose a different email."
 
 # Duplicate username
 print("Test 7 - Duplicate username :")
-reset_users_db()
-users_db.add_user("test5@example.com", "mat", "Pass234!", "Pass234!")
-print(registration())
-print()
+users_db.users = {}
+u=User("mat", "test6@example.com", "Pass234!", "Mat6", 20, "France")
+valid, msg = validate_registration(u, "Pass234!")
+assert valid is False
+assert msg == "Username already exists. Please choose a different username."
+
 
 # Spaces in username or email
 print("Test 8 - Spaces in username or email :")
-reset_users_db()
-users_db.add_user("test6@example.com", "mat 06", "Pass234!", "Pass234!")
-print(registration())
-reset_users_db()
-users_db.add_user("test 7@example.com", "mat07", "Pass234!", "Pass234!")
-print(registration())
-print()
+users_db.users = {}
+u=User("mat 07", "test7@example.com", "Pass234!", "Mat7", 20, "France")
+valid, msg = validate_registration(u, "Pass234!")
+assert valid is False    
+assert msg == "Username and email cannot contain spaces."
+
+users_db.users = {}
+u=User("mat07", "test@ example.com", "Pass234!", "Mat07", 20, "France")
+valid, msg = validate_registration(u, "Pass234!")
+assert valid is False        
+assert msg == "Username and email cannot contain spaces."
+
 
